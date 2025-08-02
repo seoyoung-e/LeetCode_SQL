c26d9f5d5d7d79b6416f296a61e3dd3b763ebd04
Seoyoung Lee <79400606+seoyoung-e@users.noreply.github.com>
2025-08-02 13:21:46 +0900

    Time: 759 ms (76.21%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -3,6 +3,8 @@ with d as (select turn, person_id, person_name, weight
     ,sum(weight) over(order by turn) as total_weight
 from queue)
 
-select distinct last_value(person_name) over(order by total_weight
-        	rows between unbounded preceding and unbounded following) as person_name
+
+
+select distinct last_value(person_name) over() as person_name
 from d where total_weight<=1000;
+
