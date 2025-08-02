7f7b0edf9517cb71f67ceedb5d923f0b5d22a5d6
Seoyoung Lee <79400606+seoyoung-e@users.noreply.github.com>
2025-08-02 12:10:58 +0900

    Time: 380 ms (89.09%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,17 @@
 select id
+from(select *,lag(recorddate) over (order by recordDate) as prevdate, lag(temperature) over(order by recordDate) as prevtemp
+from weather)c
+where temperature >  prevtemp and recordDate= date_add(prevdate, interval 1 day)
+
+
+
+
+
+
+/*select id
 from (select *, 
     lag(recordDate) over(order by recordDate) as prevdate,
     lag(temperature) over(order by recordDate) as prevtemp
     from Weather)c
 where temperature>prevtemp and recordDate=date_add(prevdate, interval 1 day)
+*/
